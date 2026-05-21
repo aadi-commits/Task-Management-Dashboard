9163f06ee1e99f0b1523c8323d8d5c88267b70b3
Adit <aditnavle@gmail.com>
2026-05-22 00:57:49 +0530

    feat: auth controller with register and login

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const User = require("./models/User");
 const Task = require("./models/Task");
+const authController = require("./controllers/authController");
 
 const app = express();
 
